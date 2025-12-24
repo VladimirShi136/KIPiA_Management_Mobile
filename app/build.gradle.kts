4062d9d2bdd123330f2631b8c1166aa6293373b9
VladimirShi136 <vovashi136@yandex.ru>
2025-12-24 17:12:56 +0300

    24/12/2025 - update libs and build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -37,13 +39,16 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
+    }
+}
+
+// Новый блок для Kotlin-компилятора
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17 // Указываем через тип JvmTarget
     }
 }
 
